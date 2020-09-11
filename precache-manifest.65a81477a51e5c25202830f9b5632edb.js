7980731cb330aef976e50454ff50a457462f6655
harshitagupta11 <guptaharshita99@gmail.com>
2020-09-11 21:30:15 +0530

    Updates

---
+++ precache-manifest.65a81477a51e5c25202830f9b5632edb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efa0ce034783290963127a266c02abe4",
+    "url": "/youtube-video-player/index.html"
+  },
+  {
+    "revision": "ca6f77501c7dbacf6f42",
+    "url": "/youtube-video-player/static/css/main.575d2320.chunk.css"
+  },
+  {
+    "revision": "d423ef61eef4aa42dff2",
+    "url": "/youtube-video-player/static/js/2.2b6b49a8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/youtube-video-player/static/js/2.2b6b49a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca6f77501c7dbacf6f42",
+    "url": "/youtube-video-player/static/js/main.5c35ca67.chunk.js"
+  },
+  {
+    "revision": "0732dc51cf7081ae2925",
+    "url": "/youtube-video-player/static/js/runtime-main.6f1f8e81.js"
+  }
+]);
